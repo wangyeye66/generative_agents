2986548508e8b958ec87de4b8479c0e2de4eb4ed
wangyeye66 <1391544274@qq.com>
2024-04-01 23:22:45 -0400

    Refactor code for improved performance

--- reverie/backend_server/persona/prompt_template/gpt_structure.py
+++ reverie/backend_server/persona/prompt_template/gpt_structure.py
@@ -6,7 +6,7 @@ Description: Wrapper functions for calling OpenAI APIs.
 """
 import json
 import random
-import openai
+# import openai
 import time 
 
 from utils import *
@@ -17,178 +17,178 @@ import ollama
 def temp_sleep(seconds=0.1):
   time.sleep(seconds)
 
-def ChatGPT_single_request(prompt): 
-  temp_sleep()
+# def ChatGPT_single_request(prompt): 
+#   temp_sleep()
 
-  completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo", 
-    messages=[{"role": "user", "content": prompt}]
-  )
-  return completion["choices"][0]["message"]["content"]
+#   completion = openai.ChatCompletion.create(
+#     model="gpt-3.5-turbo", 
+#     messages=[{"role": "user", "content": prompt}]
+#   )
+#   return completion["choices"][0]["message"]["content"]
 
 
 # ============================================================================
 # #####################[SECTION 1: CHATGPT-3 STRUCTURE] ######################
 # ============================================================================
 
-def GPT4_request(prompt): 
-  """
-  Given a prompt and a dictionary of GPT parameters, make a request to OpenAI
-  server and returns the response. 
-  ARGS:
-    prompt: a str prompt
-    gpt_parameter: a python dictionary with the keys indicating the names of  
-                   the parameter and the values indicating the parameter 
-                   values.   
-  RETURNS: 
-    a str of GPT-3's response. 
-  """
-  temp_sleep()
+# def GPT4_request(prompt): 
+#   """
+#   Given a prompt and a dictionary of GPT parameters, make a request to OpenAI
+#   server and returns the response. 
+#   ARGS:
+#     prompt: a str prompt
+#     gpt_parameter: a python dictionary with the keys indicating the names of  
+#                    the parameter and the values indicating the parameter 
+#                    values.   
+#   RETURNS: 
+#     a str of GPT-3's response. 
+#   """
+#   temp_sleep()
 
-  try: 
-    completion = openai.ChatCompletion.create(
-    model="gpt-4", 
-    messages=[{"role": "user", "content": prompt}]
-    )
-    return completion["choices"][0]["message"]["content"]
+#   try: 
+#     completion = openai.ChatCompletion.create(
+#     model="gpt-4", 
+#     messages=[{"role": "user", "content": prompt}]
+#     )
+#     return completion["choices"][0]["message"]["content"]
   
-  except: 
-    print ("ChatGPT ERROR")
-    return "ChatGPT ERROR"
+#   except: 
+#     print ("ChatGPT ERROR")
+#     return "ChatGPT ERROR"
 
 
-def ChatGPT_request(prompt): 
-  """
-  Given a prompt and a dictionary of GPT parameters, make a request to OpenAI
-  server and returns the response. 
-  ARGS:
-    prompt: a str prompt
-    gpt_parameter: a python dictionary with the keys indicating the names of  
-                   the parameter and the values indicating the parameter 
-                   values.   
-  RETURNS: 
-    a str of GPT-3's response. 
-  """
-  # temp_sleep()
-  try: 
-    completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo", 
-    messages=[{"role": "user", "content": prompt}]
-    )
-    return completion["choices"][0]["message"]["content"]
+# def ChatGPT_request(prompt): 
+#   """
+#   Given a prompt and a dictionary of GPT parameters, make a request to OpenAI
+#   server and returns the response. 
+#   ARGS:
+#     prompt: a str prompt
+#     gpt_parameter: a python dictionary with the keys indicating the names of  
+#                    the parameter and the values indicating the parameter 
+#                    values.   
+#   RETURNS: 
+#     a str of GPT-3's response. 
+#   """
+#   # temp_sleep()
+#   try: 
+#     completion = openai.ChatCompletion.create(
+#     model="gpt-3.5-turbo", 
+#     messages=[{"role": "user", "content": prompt}]
+#     )
+#     return completion["choices"][0]["message"]["content"]
   
-  except: 
-    print ("ChatGPT ERROR")
-    return "ChatGPT ERROR"
+#   except: 
+#     print ("ChatGPT ERROR")
+#     return "ChatGPT ERROR"
 
 
-def GPT4_safe_generate_response(prompt, 
-                                   example_output,
-                                   special_instruction,
-                                   repeat=3,
-                                   fail_safe_response="error",
-                                   func_validate=None,
-                                   func_clean_up=None,
-                                   verbose=False): 
-  prompt = 'GPT-3 Prompt:\n"""\n' + prompt + '\n"""\n'
-  prompt += f"Output the response to the prompt above in json. {special_instruction}\n"
-  prompt += "Example output json:\n"
-  prompt += '{"output": "' + str(example_output) + '"}'
+# def GPT4_safe_generate_response(prompt, 
+#                                    example_output,
+#                                    special_instruction,
+#                                    repeat=3,
+#                                    fail_safe_response="error",
+#                                    func_validate=None,
+#                                    func_clean_up=None,
+#                                    verbose=False): 
+#   prompt = 'GPT-3 Prompt:\n"""\n' + prompt + '\n"""\n'
+#   prompt += f"Output the response to the prompt above in json. {special_instruction}\n"
+#   prompt += "Example output json:\n"
+#   prompt += '{"output": "' + str(example_output) + '"}'
 
-  if verbose: 
-    print ("CHAT GPT PROMPT")
-    print (prompt)
+#   if verbose: 
+#     print ("CHAT GPT PROMPT")
+#     print (prompt)
 
-  for i in range(repeat): 
+#   for i in range(repeat): 
 
-    try: 
-      curr_gpt_response = GPT4_request(prompt).strip()
-      end_index = curr_gpt_response.rfind('}') + 1
-      curr_gpt_response = curr_gpt_response[:end_index]
-      curr_gpt_response = json.loads(curr_gpt_response)["output"]
+#     try: 
+#       curr_gpt_response = GPT4_request(prompt).strip()
+#       end_index = curr_gpt_response.rfind('}') + 1
+#       curr_gpt_response = curr_gpt_response[:end_index]
+#       curr_gpt_response = json.loads(curr_gpt_response)["output"]
       
-      if func_validate(curr_gpt_response, prompt=prompt): 
-        return func_clean_up(curr_gpt_response, prompt=prompt)
+#       if func_validate(curr_gpt_response, prompt=prompt): 
+#         return func_clean_up(curr_gpt_response, prompt=prompt)
       
-      if verbose: 
-        print ("---- repeat count: \n", i, curr_gpt_response)
-        print (curr_gpt_response)
-        print ("~~~~")
+#       if verbose: 
+#         print ("---- repeat count: \n", i, curr_gpt_response)
+#         print (curr_gpt_response)
+#         print ("~~~~")
 
-    except: 
-      pass
+#     except: 
+#       pass
 
-  return False
+#   return False
 
 
-def ChatGPT_safe_generate_response(prompt, 
-                                   example_output,
-                                   special_instruction,
-                                   repeat=3,
-                                   fail_safe_response="error",
-                                   func_validate=None,
-                                   func_clean_up=None,
-                                   verbose=False): 
-  # prompt = 'GPT-3 Prompt:\n"""\n' + prompt + '\n"""\n'
-  prompt = '"""\n' + prompt + '\n"""\n'
-  prompt += f"Output the response to the prompt above in json. {special_instruction}\n"
-  prompt += "Example output json:\n"
-  prompt += '{"output": "' + str(example_output) + '"}'
+# def ChatGPT_safe_generate_response(prompt, 
+#                                    example_output,
+#                                    special_instruction,
+#                                    repeat=3,
+#                                    fail_safe_response="error",
+#                                    func_validate=None,
+#                                    func_clean_up=None,
+#                                    verbose=False): 
+#   # prompt = 'GPT-3 Prompt:\n"""\n' + prompt + '\n"""\n'
+#   prompt = '"""\n' + prompt + '\n"""\n'
+#   prompt += f"Output the response to the prompt above in json. {special_instruction}\n"
+#   prompt += "Example output json:\n"
+#   prompt += '{"output": "' + str(example_output) + '"}'
 
-  if verbose: 
-    print ("CHAT GPT PROMPT")
-    print (prompt)
+#   if verbose: 
+#     print ("CHAT GPT PROMPT")
+#     print (prompt)
 
-  for i in range(repeat): 
+#   for i in range(repeat): 
 
-    try: 
-      curr_gpt_response = ChatGPT_request(prompt).strip()
-      end_index = curr_gpt_response.rfind('}') + 1
-      curr_gpt_response = curr_gpt_response[:end_index]
-      curr_gpt_response = json.loads(curr_gpt_response)["output"]
+#     try: 
+#       curr_gpt_response = ChatGPT_request(prompt).strip()
+#       end_index = curr_gpt_response.rfind('}') + 1
+#       curr_gpt_response = curr_gpt_response[:end_index]
+#       curr_gpt_response = json.loads(curr_gpt_response)["output"]
 
-      # print ("---ashdfaf")
-      # print (curr_gpt_response)
-      # print ("000asdfhia")
+#       # print ("---ashdfaf")
+#       # print (curr_gpt_response)
+#       # print ("000asdfhia")
       
-      if func_validate(curr_gpt_response, prompt=prompt): 
-        return func_clean_up(curr_gpt_response, prompt=prompt)
+#       if func_validate(curr_gpt_response, prompt=prompt): 
+#         return func_clean_up(curr_gpt_response, prompt=prompt)
       
-      if verbose: 
-        print ("---- repeat count: \n", i, curr_gpt_response)
-        print (curr_gpt_response)
-        print ("~~~~")
+#       if verbose: 
+#         print ("---- repeat count: \n", i, curr_gpt_response)
+#         print (curr_gpt_response)
+#         print ("~~~~")
 
-    except: 
-      pass
+#     except: 
+#       pass
 
-  return False
+#   return False
 
 
-def ChatGPT_safe_generate_response_OLD(prompt, 
-                                   repeat=3,
-                                   fail_safe_response="error",
-                                   func_validate=None,
-                                   func_clean_up=None,
-                                   verbose=False): 
-  if verbose: 
-    print ("CHAT GPT PROMPT")
-    print (prompt)
+# def ChatGPT_safe_generate_response_OLD(prompt, 
+#                                    repeat=3,
+#                                    fail_safe_response="error",
+#                                    func_validate=None,
+#                                    func_clean_up=None,
+#                                    verbose=False): 
+#   if verbose: 
+#     print ("CHAT GPT PROMPT")
+#     print (prompt)
 
-  for i in range(repeat): 
-    try: 
-      curr_gpt_response = ChatGPT_request(prompt).strip()
-      if func_validate(curr_gpt_response, prompt=prompt): 
-        return func_clean_up(curr_gpt_response, prompt=prompt)
-      if verbose: 
-        print (f"---- repeat count: {i}")
-        print (curr_gpt_response)
-        print ("~~~~")
+#   for i in range(repeat): 
+#     try: 
+#       curr_gpt_response = ChatGPT_request(prompt).strip()
+#       if func_validate(curr_gpt_response, prompt=prompt): 
+#         return func_clean_up(curr_gpt_response, prompt=prompt)
+#       if verbose: 
+#         print (f"---- repeat count: {i}")
+#         print (curr_gpt_response)
+#         print ("~~~~")
 
-    except: 
-      pass
-  print ("FAIL SAFE TRIGGERED") 
-  return fail_safe_response
+#     except: 
+#       pass
+#   print ("FAIL SAFE TRIGGERED") 
+#   return fail_safe_response
 
 
 # ============================================================================
@@ -197,36 +197,36 @@ def ChatGPT_safe_generate_response_OLD(prompt,
 
 def LLaMA_request(prompt, llama_parameter):
     temp_sleep()
-    response = ollama.generate(model='llama2:70b', prompt=prompt, **llama_parameter)
+    response = ollama.generate(model='llama2', prompt=prompt, **llama_parameter)
     return response["response"]
 
 
-def generate_prompt(curr_input, prompt_lib_file): 
-  """
-  Takes in the current input (e.g. comment that you want to classifiy) and 
-  the path to a prompt file. The prompt file contains the raw str prompt that
-  will be used, which contains the following substr: !<INPUT>! -- this 
-  function replaces this substr with the actual curr_input to produce the 
-  final promopt that will be sent to the GPT3 server. 
-  ARGS:
-    curr_input: the input we want to feed in (IF THERE ARE MORE THAN ONE
-                INPUT, THIS CAN BE A LIST.)
-    prompt_lib_file: the path to the promopt file. 
-  RETURNS: 
-    a str prompt that will be sent to OpenAI's GPT server.  
-  """
-  if type(curr_input) == type("string"): 
-    curr_input = [curr_input]
-  curr_input = [str(i) for i in curr_input]
+# def generate_prompt(curr_input, prompt_lib_file): 
+#   """
+#   Takes in the current input (e.g. comment that you want to classifiy) and 
+#   the path to a prompt file. The prompt file contains the raw str prompt that
+#   will be used, which contains the following substr: !<INPUT>! -- this 
+#   function replaces this substr with the actual curr_input to produce the 
+#   final promopt that will be sent to the GPT3 server. 
+#   ARGS:
+#     curr_input: the input we want to feed in (IF THERE ARE MORE THAN ONE
+#                 INPUT, THIS CAN BE A LIST.)
+#     prompt_lib_file: the path to the promopt file. 
+#   RETURNS: 
+#     a str prompt that will be sent to OpenAI's GPT server.  
+#   """
+#   if type(curr_input) == type("string"): 
+#     curr_input = [curr_input]
+#   curr_input = [str(i) for i in curr_input]
 
-  f = open(prompt_lib_file, "r")
-  prompt = f.read()
-  f.close()
-  for count, i in enumerate(curr_input):   
-    prompt = prompt.replace(f"!<INPUT {count}>!", i)
-  if "<commentblockmarker>###</commentblockmarker>" in prompt: 
-    prompt = prompt.split("<commentblockmarker>###</commentblockmarker>")[1]
-  return prompt.strip()
+#   f = open(prompt_lib_file, "r")
+#   prompt = f.read()
+#   f.close()
+#   for count, i in enumerate(curr_input):   
+#     prompt = prompt.replace(f"!<INPUT {count}>!", i)
+#   if "<commentblockmarker>###</commentblockmarker>" in prompt: 
+#     prompt = prompt.split("<commentblockmarker>###</commentblockmarker>")[1]
+#   return prompt.strip()
 
 
 def safe_generate_response(prompt, 
